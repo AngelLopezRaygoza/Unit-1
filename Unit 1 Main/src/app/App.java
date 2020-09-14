0f81f3bc9dfd4ee6f6824667d3c7c760e78759aa
AngelLopezRaygoza <819840@mystma.org>
2020-09-14 22:35:33 +0000

    Added Top Five Movies (Fixed)

--- Unit 1 Main/src/app/App.java
+++ Unit 1 Main/src/app/App.java
@@ -3,5 +3,10 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Mr. Osowski is the worst teacher");
+        System.out.println("1. Parasite");
+        System.out.println("2. Doctor Strange");
+        System.out.println("3. Percy Jackson and the Olympians");
+        System.out.println("4. Avengers Endgame");
+        System.out.println("5. Percy Jackson: Sea of Monsters");
     }
 }
