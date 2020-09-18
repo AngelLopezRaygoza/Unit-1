c48a40c91a188d30e487d4e59b949fa37bcec37c
AngelLopezRaygoza <819840@mystma.org>
2020-09-18 21:53:27 +0000

    Variable & Data Types

--- Unit 1 Main/src/app/App.java
+++ Unit 1 Main/src/app/App.java
@@ -2,11 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Mr. Osowski is the worst teacher");
-        System.out.println("1. Parasite");
-        System.out.println("2. Doctor Strange");
-        System.out.println("3. Percy Jackson and the Olympians");
-        System.out.println("4. Avengers Endgame");
-        System.out.println("5. Percy Jackson: Sea of Monsters");
+       boolean iHavADog = true;
+       byte howManFingIHav = 10;
+       char secLetOfAlph = 'b';
+       Short howManGuitIHav = 2;
+       int howOldAmI = 17;
+       long numOfStatInUsa = 50;
+       float howManClassDoIHav = 5;
+       double howTalAmI = 5.5;
+       
+       System.out.println("How Many Classes Do I Have");
+       System.out.println(howManClassDoIHav);
+       System.out.println("How Tall Am I");
+       System.out.println(howTalAmI);
+       System.out.println("Number Of States In The USA");
+       System.out.println(numOfStatInUsa);
+       System.out.println("How OLd Am I");
+       System.out.println(howOldAmI);
+       System.out.println("How Many Guitars Do I Have");
+       System.out.println(howManGuitIHav);
+       System.out.println("Second Letter Of The Alphabet");
+       System.out.println(secLetOfAlph);
+       System.out.println("How Many Fingers Do I Have");
+       System.out.println(howManFingIHav);
+       System.out.println("I Have A Dog");
+       System.out.println(iHavADog);
     }
 }
