293a138c9b7a5020958f1b65388ef8889395b22e
AngelLopezRaygoza <819840@mystma.org>
2020-09-21 20:39:46 +0000

    Concatenation and Strings

--- Unit 1 Main/src/app/App.java
+++ Unit 1 Main/src/app/App.java
@@ -2,30 +2,23 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       boolean iHavADog = true;
-       byte howManFingIHav = 10;
-       char secLetOfAlph = 'b';
-       Short howManGuitIHav = 2;
-       int howOldAmI = 17;
-       long numOfStatInUsa = 50;
-       float howManClassDoIHav = 5;
-       double howTalAmI = 5.5;
-       
-       System.out.println("How Many Classes Do I Have");
-       System.out.println(howManClassDoIHav);
-       System.out.println("How Tall Am I");
-       System.out.println(howTalAmI);
-       System.out.println("Number Of States In The USA");
-       System.out.println(numOfStatInUsa);
-       System.out.println("How OLd Am I");
-       System.out.println(howOldAmI);
-       System.out.println("How Many Guitars Do I Have");
-       System.out.println(howManGuitIHav);
-       System.out.println("Second Letter Of The Alphabet");
-       System.out.println(secLetOfAlph);
-       System.out.println("How Many Fingers Do I Have");
-       System.out.println(howManFingIHav);
-       System.out.println("I Have A Dog");
-       System.out.println(iHavADog);
+      String person1 = "Mike";
+      String person2 = "Sam";
+      String verb1 = "thinks";
+      String verb2 = "agrees";
+      String verb3 = "think";
+      String food = "pizza";
+      String dayOfWeek = "Friday";
+      String place1 = "home";
+      String place2 = "school";
+      String adjective1 = "boring";
+      String adjective2 = "little";
+
+      System.out.println(person1 + " " + verb1 + " " + "school is" + " " + adjective1);
+      System.out.println(person2 + " " + verb2 + " " + "with" + " " + person1);
+      System.out.println("But I" + " " + verb3 + " " + "online school is a" + " " + adjective2 + " " + "better, Mike says");
+      System.out.println("At" + " " + place1 + " " + "we get to eat whenever we want");
+      System.out.println("The only good food at" + " " + place2 +" " + "was" + " " + food + " " + "which was served every" + " " + dayOfWeek);
+
     }
 }
