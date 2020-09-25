1d18e08af3e533f54d1e0a78768dbde137c7b1ef
AngelLopezRaygoza <819840@mystma.org>
2020-09-25 00:50:36 +0000

    Getting User Input

--- Unit 1 Main/src/app/App.java
+++ Unit 1 Main/src/app/App.java
@@ -1,24 +1,23 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      String person1 = "Mike";
-      String person2 = "Sam";
-      String verb1 = "thinks";
-      String verb2 = "agrees";
-      String verb3 = "think";
-      String food = "pizza";
-      String dayOfWeek = "Friday";
-      String place1 = "home";
-      String place2 = "school";
-      String adjective1 = "boring";
-      String adjective2 = "little";
+      
+        Scanner input = new Scanner(System.in);
 
-      System.out.println(person1 + " " + verb1 + " " + "school is" + " " + adjective1);
-      System.out.println(person2 + " " + verb2 + " " + "with" + " " + person1);
-      System.out.println("But I" + " " + verb3 + " " + "online school is a" + " " + adjective2 + " " + "better, Mike says");
-      System.out.println("At" + " " + place1 + " " + "we get to eat whenever we want");
-      System.out.println("The only good food at" + " " + place2 +" " + "was" + " " + food + " " + "which was served every" + " " + dayOfWeek);
+        System.out.println("Please enter you first name.");
+        String firstName = input.nextLine();
+        System.out.println("Please enter your last name.");
+        String lastName = input.nextLine();
+        System.out.println("Please enter your age.");
+        String yourAge = input.nextLine(); 
+        System.out.println("Please enter your grade.");
+        String yourGrade = input.nextLine();
+        System.out.println("Please enter your favorite color.");
+        String favColor = input.nextLine();
+
+        System.out.println("Hello, " + firstName + " " + lastName + ", your are " + yourAge + ", you are a " + yourGrade + ", and your favorite color is " + favColor);
 
     }
 }
