a305219fd92fe97cffc900e1e37328a891e95704
AngelLopezRaygoza <819840@mystma.org>
2020-10-21 16:12:49 +0000

    Challenge 3

--- Unit 1 Main/src/app/App.java
+++ Unit 1 Main/src/app/App.java
@@ -6,45 +6,35 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        
-        for (int i = 0; i <= 1000; i++) {
-            System.out.println(i);
-        }
+    
 
-        System.out.println("Enter the correct password for the secret message");
-        System.out.println("(Hint, it's four numbers long).");
-        
-        int i = 0;
+    Boolean isFinished = false;
 
-        String passwordGuess = input.nextLine();
+    int correctAns = 7;
 
-        String correctPassword = "1234";
 
-        if (passwordGuess.equals(correctPassword)) {
-            System.out.println("Why are we still here? Just to suffer? Every night, I can feel my leg… and my arm… even my fingers. The body I’ve lost… the comrades I’ve lost… won’t stop hurting… It’s like they’re all still there. You feel it, too, don’t you?");
+    while (isFinished == false) {
+
+System.out.println("Guess the correct number between 0-100");
+
+int guess = input.nextInt();
+
+        if (guess == correctAns) {
+        System.out.println("Correct!");
+        isFinished = true;
+    }
+    else{
+        if (guess > correctAns) {
+            System.out.println("High");
         }
         else {
-            System.out.println("Incorrect password, please try again");
-        if (i <= 0);{ 
-        String passwordGuess2 = input.nextLine();
-        if (passwordGuess2.equals(correctPassword)) {
-            System.out.println("Why are we still here? Just to suffer? Every night, I can feel my leg… and my arm… even my fingers. The body I’ve lost… the comrades I’ve lost… won’t stop hurting… It’s like they’re all still there. You feel it, too, don’t you?");
+            System.out.println("Low");
         }
-        else {
-            System.out.println("Incorrect password, please try again");
+    }
+    }
+
+  
+
         
-        
-        if (i <= 0);{ 
-        String passwordGuess3 = input.nextLine();
-        if (passwordGuess3.equals(correctPassword)) {
-            System.out.println("Why are we still here? Just to suffer? Every night, I can feel my leg… and my arm… even my fingers. The body I’ve lost… the comrades I’ve lost… won’t stop hurting… It’s like they’re all still there. You feel it, too, don’t you?");
-        }
-        else {
-            System.out.println("Incorrect password");
-        }
-        }
-}
-}   
-}
-}
+ }
 }
